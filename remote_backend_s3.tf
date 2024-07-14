f4d2b8fcb5b6eb1acf4a864280b3345b3223e7d2
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-14 15:14:21 -0600

    ami updated

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "dev-proj-1-jenkins-remote-state-bucket-77777"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "us-east-1"
   }
-}
+}*/
